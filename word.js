904f31cdc0a611efc3863b733d706aee912a755a
Anuradha Bansode <121301273+anyalisis12@users.noreply.github.com>
2024-08-24 13:15:42 +0530

    Add files via upload

---
+++ word.js
@@ -0,0 +1,199 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument with some strings"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "amazon",
+        hint: "online shopping site"
+    },
+    {
+        word: "clock",
+        hint: "there is something that shows time"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+    {
+        word: "history",
+        hint: "The study of past events and human civilization."
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "cricket",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to an brain game"
+    },
+    {
+        word: "Instagram",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "jaipur",
+        hint: "famous pink city"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "Sushi",
+        hint: "Famous japanese dish"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body."
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "waterfall",
+        hint: "A cascade of water falling from a height."
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "oxygen",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "universe",
+        hint: "All existing matter, space, and time as a whole."
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "sunflower",
+        hint: "A tall plant with a large yellow flower head."
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "country name Famous for Gold"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
